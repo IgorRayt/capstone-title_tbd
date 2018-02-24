b3fd7dd2a9839222bab555237fb3e8b9cc9e0c85
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-02-23 20:59:51 -0500

    add oauth2 authentication

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,10 +2,12 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 // https://spring.io/guides/gs/accessing-data-mysql/
 
 @SpringBootApplication
+@EnableResourceServer
 public class Application {
 
     public static void main(String[] args) {
