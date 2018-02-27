a6c10771a3965d0e4f11fcb63f4fc6e766a5df37
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-02-27 18:54:25 -0500

    implemented Igor's validation for JobController

--- src/main/java/hello/controller/JobController.java
+++ src/main/java/hello/controller/JobController.java
@@ -32,9 +32,30 @@ public class JobController {
     private CustomerRepository customerRepository;
 
     @PostMapping("")
-    public @ResponseBody
-    Job createJob(@Valid @RequestBody Job job) {
-        return jobRepository.save(job);
+    public ResponseEntity<?> createJob(@Valid @RequestBody Job job) {
+        String error = "";
+        if (job.getDateOpened() == null) {
+            error += "Date opened cannot be null";
+        } else {
+            if (job.getDateOpened().getTime() < System.currentTimeMillis()) {
+                error += "Job cannot be opened in the past\n";
+            }
+            if (job.getDateClosed() != null && job.getDateOpened().getTime() > job.getDateClosed().getTime()) {
+                error += "Job open date cannot be after the job close date\n";
+            }
+        }
+
+        if (job.getDescription() == null || job.getDescription().equals("")) {
+            error += "Job description cannot be empty\n";
+        }
+        if (job.getAvailable() == null) {
+            job.setAvailable(false);
+        }
+
+        if (!error.equals("")) {
+            return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.ok(jobRepository.save(job));
     }
 
     @GetMapping(path = {"", "/all"})
@@ -55,8 +76,31 @@ public class JobController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Job> updateJob(@PathVariable(value = "id") Long jobId,
-                                         @Valid @RequestBody Job jobDetails) {
+    public ResponseEntity<?> updateJob(@PathVariable(value = "id") Long jobId,
+                                       @Valid @RequestBody Job jobDetails) {
+        String error = "";
+        if (jobDetails.getDateOpened() == null) {
+            error += "Date opened cannot be null";
+        } else {
+            if (jobDetails.getDateOpened().getTime() < System.currentTimeMillis()) {
+                error += "Job cannot be opened in the past\n";
+            }
+            if (jobDetails.getDateClosed() != null && jobDetails.getDateOpened().getTime() > jobDetails.getDateClosed().getTime()) {
+                error += "Job open date cannot be after the job close date\n";
+            }
+        }
+
+        if (jobDetails.getDescription() == null || jobDetails.getDescription().equals("")) {
+            error += "Job description cannot be empty\n";
+        }
+        if (jobDetails.getAvailable() == null) {
+            jobDetails.setAvailable(false);
+        }
+
+        if (!error.equals("")) {
+            return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        }
+
         Optional<Job> optionalJob = jobRepository.findById(jobId);
         if (!optionalJob.isPresent()) {
             return ResponseEntity.notFound().build();
@@ -64,6 +108,7 @@ public class JobController {
         Job job = optionalJob.get();
 
         Job updatedJob = jobRepository.save(job.merge(jobDetails));
+
         return ResponseEntity.ok(updatedJob);
     }
 
@@ -79,6 +124,24 @@ public class JobController {
         Job job = optionalJob.get();
 
         job.setAvailable(true);
+
+        Job updatedJob = jobRepository.save(job);
+
+        return ResponseEntity.ok(updatedJob);
+    }
+
+    // set a job as unavailable
+    @PutMapping("{id}/unavailable")
+    public ResponseEntity<Job> setJobUnavailable(
+            @PathVariable(value = "id") Long jobId
+    ) {
+        Optional<Job> optionalJob = jobRepository.findById(jobId);
+        if (!optionalJob.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        Job job = optionalJob.get();
+
+        job.setAvailable(false);
 
         Job updatedJob = jobRepository.save(job);
 
@@ -110,7 +173,7 @@ public class JobController {
         return ResponseEntity.ok(updatedJob);
     }
 
-    // add an employee to a job
+    // add a customer to a job
     @PutMapping("{id}/customer/{customerId}")
     public ResponseEntity<Job> addCustomerToJob(
             @PathVariable(value = "id") Long jobId,
@@ -141,7 +204,22 @@ public class JobController {
             @PathVariable(value = "id") Long jobId,
             @PathVariable(value = "employeeId") Long employeeId
     ) {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(null);
+        Optional<Job> optionalJob = jobRepository.findById(jobId);
+        if (!optionalJob.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        Job job = optionalJob.get();
+
+        Optional<Employee> optionalEmployee= employeeRepository.findById(employeeId);
+        if(!optionalEmployee.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        Employee employee = optionalEmployee.get();
+
+        job.getEmployees().remove(employee);
+
+        Job updatedJob = jobRepository.save(job);
+        return ResponseEntity.ok(updatedJob);
     }
 
     @DeleteMapping("{id}")
